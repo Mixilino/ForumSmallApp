16cf72d115663e2d8641e703cefd28305864aa45
Mixilino <nisavicmiha@gmail.com>
2023-03-09 13:26:14 +0100

    summary for serviceresponse

--- ForumWebApi/Models/ServiceResponse.cs
+++ ForumWebApi/Models/ServiceResponse.cs
@@ -4,10 +4,25 @@ using System.Xml.Linq;
 
 namespace ForumWebApi.Models
 {
+    /// <summary>
+    /// Represents a response from a service operation, which can contain data of type T and a message.
+    /// </summary>
+    /// <typeparam name="T">The type of the data that will be returned in response.</typeparam>
     public class ServiceResponse<T>
     {
+        /// <summary>
+        /// Gets or sets the data that will be returned in response. This property can be null.
+        /// </summary>
         public T? Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the service operation was successful or not.
+        /// </summary>
         public bool Succes { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a message that describes the result of the service operation.
+        /// </summary>
         public string Message { get; set; } = string.Empty;
 
         public override bool Equals(object obj)
