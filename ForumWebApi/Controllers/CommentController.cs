8c280d4d9606ce72147f7e49bc1015c265d7d098
Mixilino <nisavicmiha@gmail.com>
2023-03-09 13:46:37 +0100

    Documentation for Auth, Comment and post categories controllers

--- ForumWebApi/Controllers/CommentController.cs
+++ ForumWebApi/Controllers/CommentController.cs
@@ -11,6 +11,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ForumWebApi.Controllers
 {
+
+    /// <summary>
+    /// A controller class for managing comments in an API. Client needs to be Authorized to access any of method in this class
+    /// </summary>
     [Authorize]
     [Route("api/comments")]
     [ApiController]
@@ -23,6 +27,11 @@ namespace ForumWebApi.Controllers
             this.commentService = commentService;
         }
 
+        /// <summary>
+        /// Creates a new comment. Only Admin and Regular user can access this endpoint.
+        /// </summary>
+        /// <param name="commentDto">The comment data that is submitted in the request body.</param>
+        /// <returns>An ActionResult of type ServiceResponse<CommentResponseDto>, which contains information about the success or failure of the operation.</returns>
         [Authorize(Roles = "Admin,Regular")]
         [HttpPost("create")]
         public ActionResult<ServiceResponse<CommentResponseDto>> Create([FromBody] CommentCreateDto commentDto)
@@ -42,7 +51,11 @@ namespace ForumWebApi.Controllers
             return BadRequest(p);
         }
 
-
+        /// <summary>
+        /// Deletes a comment. Only Admin and Regular user can access this endpoint.
+        /// </summary>
+        /// <param name="CommentId">The ID of the comment that is to be deleted.</param>
+        /// <returns>An ActionResult of type ServiceResponse<CommentResponseDto>, which contains information about the success or failure of the operation.</returns>
         [Authorize(Roles = "Admin,Regular")]
         [HttpDelete("delete")]
         public ActionResult<ServiceResponse<CommentResponseDto>> Delete([FromQuery] int CommentId)
@@ -62,6 +75,11 @@ namespace ForumWebApi.Controllers
             return BadRequest(p);
         }
 
+        /// <summary>
+        /// Changes an existing comment. Only Admin and Regular user can access this endpoint.
+        /// </summary>
+        /// <param name="commentChangeDto">The comment data that is submitted in the request body.</param>
+        /// <returns>An ActionResult of type ServiceResponse<CommentResponseDto>, which contains information about the success or failure of the operation.</returns>
         [Authorize(Roles = "Admin,Regular")]
         [HttpPatch("edit")]
         public ActionResult<ServiceResponse<CommentResponseDto>> Change(CommentChangeDto commentChangeDto)
